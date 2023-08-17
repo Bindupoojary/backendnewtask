fc3c4b95bfd88d58a95e75d784d6a142d42cf9ea
unknown <bindupoojary09@gmail.com>
2023-08-17 11:50:38 +0530

    new

--- twelfth_config_form_alter/advanced/src/Form/SettingsForm.php
+++ twelfth_config_form_alter/advanced/src/Form/SettingsForm.php
@@ -4,11 +4,51 @@ namespace Drupal\advanced\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Messenger\MessengerInterface;
 
 /**
  * Configure advanced settings for this site.
  */
 class SettingsForm extends ConfigFormBase {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The messenger service.
+   *
+   * @var \Drupal\Core\Messenger\MessengerInterface
+   */
+  protected $messenger;
+
+  /**
+   * Constructs a new SettingsForm object.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   * @param \Drupal\Core\Messenger\MessengerInterface $messenger
+   *   The messenger service.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, MessengerInterface $messenger) {
+    $this->entityTypeManager = $entity_type_manager;
+    $this->messenger = $messenger;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager'),
+      $container->get('messenger')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -27,31 +67,30 @@ class SettingsForm extends ConfigFormBase {
   /**
    * {@inheritdoc}
    */
-  public function buildForm(array $form, FormStateInterface $form_state,) {
-
-    $config = \Drupal::config('advanced.settings');
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('advanced.settings');
     $tag_value = $config->get('tags');
-
 
     $form['title'] = [
       '#type' => 'textfield',
-      '#title' => $this->t('title'),
+      '#title' => $this->t('Title'),
       '#default_value' => $config->get('title'),
-
     ];
+
     $form['advanced'] = [
       '#type' => 'checkbox',
-      '#title' => $this->t('advanced'),
+      '#title' => $this->t('Advanced'),
       '#default_value' => $config->get('advanced'),
-
     ];
+
     $form['tags'] = [
       '#type' => 'entity_autocomplete',
-      '#title' => $this->t('tags'),
+      '#title' => $this->t('Tags'),
       '#target_type' => 'taxonomy_term',
-      '#default_value' => \Drupal::entityTypeManager()->getStorage('taxonomy_term')->load($tag_value),
+      '#default_value' => $this->entityTypeManager->getStorage('taxonomy_term')->load($tag_value),
       '#required' => TRUE,
     ];
+
     return parent::buildForm($form, $form_state);
   }
 
@@ -64,6 +103,8 @@ class SettingsForm extends ConfigFormBase {
       ->set('advanced', $form_state->getValue('advanced'))
       ->set('tags', $form_state->getValue('tags'))
       ->save();
+
+    $this->messenger->addStatus($this->t('Configuration saved successfully.'));
     parent::submitForm($form, $form_state);
   }
 
