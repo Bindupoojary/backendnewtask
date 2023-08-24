170445154ff439afe58c854edbcc26db7f0afa75
unknown <bindupoojary09@gmail.com>
2023-08-24 09:12:03 +0530

    new

--- seventeen_drush_task/drushtask/src/Commands/CustomDrush.php
+++ seventeen_drush_task/drushtask/src/Commands/CustomDrush.php
@@ -26,7 +26,7 @@ class CustomDrush extends DrushCommands {
   protected $state;
 
   /**
-   * Constructor for the DrushHelpersCommands class.
+   * Constructor for the CustomDrush class.
    *
    * @param \Drupal\Core\Messenger\MessengerInterface $messenger
    *   The messenger service.
@@ -36,7 +36,6 @@ class CustomDrush extends DrushCommands {
   public function __construct(MessengerInterface $messenger, StateInterface $state) {
     $this->messenger = $messenger;
     $this->state = $state;
-    parent::__construct();
   }
 
   /**
@@ -49,6 +48,18 @@ class CustomDrush extends DrushCommands {
   public function enableMaintenance() {
     $this->state->set('system.maintenance_mode', TRUE);
     $this->messenger->addStatus('Site is now in maintenance mode.');
+  }
+
+  /**
+   * Take the site out of maintenance mode.
+   *
+   * @command site-maintenance:disable
+   * @aliases smd
+   * @usage drush site-maintenance:disable
+   */
+  public function disableMaintenance() {
+    $this->state->set('system.maintenance_mode', FALSE);
+    $this->messenger->addStatus('Site is no longer in maintenance mode.');
   }
 
 }
